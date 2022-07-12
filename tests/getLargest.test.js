efbf0d8c5187754ad956c62c603f5986580fafc8
vivian <vivian.v18723@gmail.com>
2022-07-12 11:01:18 +0100

    add more tests

--- tests/getLargest.test.js
+++ tests/getLargest.test.js
@@ -1,12 +1,54 @@
 const getLargest = require("../getLargest");
 
+// describe() does lots of other stuff :)
+// can be nested
 describe("getLargest describing test", () => {
   test("getLargest is a function", () => {
     expect(typeof getLargest).toBe("function");
   });
 
+  describe("it handles normal inputs successfully", () => {
+    each([
+      [[1, 2, 3], 3],
+      [[9, 8, 7], 9],
+      [[12, 36, 19], 36],
+    ]).test("", () => {
+      expect(getLargest(arr).toEqual(expected));
+    });
+
+    test("", () => {
+      const result = getLargest(1, 2, 3);
+
+      expect(result).toEqual(3);
+    });
+
+    test("", () => {
+      const result = getLargest(3, 3, 3);
+
+      expect(result).toEqual(3);
+    });
+
+    test("", () => {
+      const result = getLargest(-1, -2, 3);
+
+      expect(result).toEqual(3);
+    });
+
+    test("", () => {
+      const result = getLargest(0.4, 1.5, 3);
+
+      expect(result).toEqual(3);
+    });
+  });
+
   test("getLargest func returns the largest value", () => {
     const result = getLargest(1, 2, 3);
+
+    expect(result).toEqual(3);
+  });
+
+  test("getLargest func returns the largest value", () => {
+    const result = getLargest(2, 2, 2);
 
     expect(result).toEqual(3);
   });
