18da8a2a2fefe980bdaa8e16f998b59548bd32bb
vivian <vivian.v18723@gmail.com>
2022-07-12 16:48:53 +0100

    add custom data

---
+++ dataInJs.js
@@ -0,0 +1,25 @@
+// variables
+const person = {
+  hadCoffeeToday: false,
+  howMuchCoffeeToday: 0,
+  likesCoffee: false,
+  favDrink: {
+    type: "water",
+    whereToBuy: "shop",
+    name: "Volvo",
+    avgPrice: 1.99,
+    isHealthy: true,
+    nutritionalInfo: {
+      sugars: 0,
+      calories: 0,
+      calcium: 10,
+      magnesium: 11,
+      sodium: 45,
+      potassium: 78,
+    },
+    howFrequentlyPerDay: 8,
+    hasVariations: false,
+  },
+  otherLikedDrinks: ["juice", "fizzy pop", "milkshake", "flavoured water"],
+  whoToMeetForDrink: ["Dan", "April", "Emily", "Reese"],
+};
