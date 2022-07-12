11776b7e65499404284bd3af991655fe7b9b198c
vivian <vivian.v18723@gmail.com>
2022-07-12 10:47:36 +0100

    group test together

--- tests/getLargest.test.js
+++ tests/getLargest.test.js
@@ -1,11 +1,13 @@
 const getLargest = require("../getLargest");
 
-test("getLargest is a function", () => {
-  expect(typeof getLargest).toBe("function");
-});
+describe("getLargest describing test", () => {
+  test("getLargest is a function", () => {
+    expect(typeof getLargest).toBe("function");
+  });
 
-test("getLargest func returns the largest value", () => {
-  const result = getLargest(1, 2, 3);
+  test("getLargest func returns the largest value", () => {
+    const result = getLargest(1, 2, 3);
 
-  expect(result).toEqual(3);
+    expect(result).toEqual(3);
+  });
 });
