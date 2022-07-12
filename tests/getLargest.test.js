3e847c1aa6a73c37df86d812294fb06560cfd78a
vivian <vivian.v18723@gmail.com>
2022-07-12 11:04:00 +0100

    refactor tests

--- tests/getLargest.test.js
+++ tests/getLargest.test.js
@@ -1,8 +1,9 @@
+// const each = require("jest-each");
+
 const getLargest = require("../getLargest");
 
-// describe() does lots of other stuff :)
-// can be nested
-describe("getLargest describing test", () => {
+// describe() does lots of other stuff :) & can be nested
+describe("This is the getLargest() describe test", () => {
   test("getLargest is a function", () => {
     expect(typeof getLargest).toBe("function");
   });
@@ -15,41 +16,5 @@ describe("getLargest describing test", () => {
     ]).test("", () => {
       expect(getLargest(arr).toEqual(expected));
     });
-
-    test("", () => {
-      const result = getLargest(1, 2, 3);
-
-      expect(result).toEqual(3);
-    });
-
-    test("", () => {
-      const result = getLargest(3, 3, 3);
-
-      expect(result).toEqual(3);
-    });
-
-    test("", () => {
-      const result = getLargest(-1, -2, 3);
-
-      expect(result).toEqual(3);
-    });
-
-    test("", () => {
-      const result = getLargest(0.4, 1.5, 3);
-
-      expect(result).toEqual(3);
-    });
-  });
-
-  test("getLargest func returns the largest value", () => {
-    const result = getLargest(1, 2, 3);
-
-    expect(result).toEqual(3);
-  });
-
-  test("getLargest func returns the largest value", () => {
-    const result = getLargest(2, 2, 2);
-
-    expect(result).toEqual(3);
   });
 });
